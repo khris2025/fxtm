ca012ad28f2a1b7cc7d7e4f441e5d75b30f2a1a4
no_name <no@email.com>
2026-07-28 22:18:50 +0100

    first commit

---
+++ layout/footer.php
@@ -0,0 +1,231 @@
+<!-- Footer Starts -->
+<footer class="footer">
+    <!-- Footer Top Area Starts -->
+    <div class="top-footer">
+        <div class="container">
+            <div class="row">
+                <!-- Footer Widget Starts -->
+                <div class="col-sm-4 col-md-2">
+                    <h4>Our Company</h4>
+                    <div class="menu">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About</a></li>
+                            <li><a href="packages.php">Packages</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Footer Widget Ends -->
+                <!-- Footer Widget Starts -->
+                <div class="col-sm-4 col-md-2">
+                    <h4>Help & Support</h4>
+                    <div class="menu">
+                        <ul>
+                            <li><a href="faq.php">FAQ</a></li>
+                            <li><a href="terms.php">Terms of Services</a></li>
+                            <li><a href="user/signup">Register</a></li>
+                            <li><a href="https://dasbhoard.inv.fxtm-capitalgrowth.com/login">Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Footer Widget Ends -->
+                <!-- Footer Widget Starts -->
+                <div class="col-sm-4 col-md-3">
+                    <h4>Contact Us </h4>
+                    <div class="contacts">
+                        <div>
+                            <span class="color-yellow">United States of America:</span><br>
+                            <span>New York</span><br>
+                        </div>
+                        <div>
+                            <span>support@fxtm-capitalgrowth.com </span>
+                        </div>
+                        <div>
+                            <span>Mon-Sun, 24/7</span>
+                        </div>
+                    </div>
+                    <!-- Social Media Profiles Starts -->
+                    <div class="social-footer">
+                        <ul>
+                            <li><a href="#" target="_blank"><i class="fa fa-telegram"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                    <!-- Social Media Profiles Ends -->
+                </div>
+                <!-- Footer Widget Ends -->
+                <!-- Footer Widget Starts -->
+                <div class="col-sm-12 col-md-5">
+                    <!-- Facts Starts -->
+                    <div class="facts-footer">
+                        <div>
+                            <h5>$390M+</h5>
+                            <span>Market cap</span>
+                        </div>
+                        <div>
+                            <h5>$6.9M+</h5>
+                            <span>daily transactions</span>
+                        </div>
+                        <div>
+                            <h5>230K+</h5>
+                            <span>active accounts</span>
+                        </div>
+                        <div>
+                            <h5>worldwide</h5>
+                            <span>supported countries</span>
+                        </div>
+                    </div>
+                    <!-- Facts Ends -->
+                    <hr>
+                    <!-- Supported Payment Cards Logo Starts -->
+                    <div class="payment-logos">
+                        <h4 class="payment-title">supported payment methods</h4>
+                        <img src="assets/images/icons/payment/american-express.png" alt="american-express">
+                        <img src="assets/images/icons/payment/mastercard.png" alt="mastercard">
+                        <img src="assets/images/icons/payment/visa.png" alt="visa">
+                        <img src="assets/images/icons/payment/paypal.png" alt="paypal">
+                        <img class="last" src="assets/images/icons/payment/maestro.png" alt="maestro">
+                        <i class="fa fa-bitcoin color-gold"></i>
+                    </div>
+                    <!-- Supported Payment Cards Logo Ends -->
+                    <div id="google_translate_element" style="margin-top:30px;"></div>
+
+                </div>
+                <!-- Footer Widget Ends -->
+            </div>
+        </div>
+    </div>
+    <!-- Footer Top Area Ends -->
+    <!-- Footer Bottom Area Starts -->
+    <div class="bottom-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <!-- Copyright Text Starts -->
+                    <p class="text-center" id="copyright"></p>
+
+                    <script>
+                      document.getElementById("copyright").innerHTML =
+                        `FXTM CAPITAL GROWTH &copy; ${new Date().getFullYear()} All Rights Reserved`;
+                    </script>
+                    <!-- Copyright Text Ends -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Bottom Area Ends -->
+</footer>
+<!-- Footer Ends -->
+<!-- Back To Top Starts  -->
+
+<!-- Back To Top Ends  -->
+
+<!-- Template JS Files -->
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/select2.min.js"></script>
+<script src="assets/js/owl.carousel.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/custom.js"></script>
+
+<script src="service-worker.js"></script>
+<script>
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('service-worker.js')
+                .then(registration => {
+                    console.log('Service Worker is registered', registration);
+                })
+                .catch(err => {
+                    console.error('Registration failed:', err);
+                });
+        });
+    }
+</script>
+
+<!-- Live Style Switcher JS File - only demo -->
+
+<style>
+    .mgm {
+        border-radius: 7px;
+        position: fixed;
+        z-index: 90;
+        top: 50%;
+        right: 40%;
+        background: #e8b90e;
+        padding: 20px 27px;
+        box-shadow: 0px 5px 13px 0px rgba(0, 0, 0, .3);
+    }
+
+    .mgm a {
+        font-weight: 700;
+        display: block;
+        color: #fff;
+    }
+
+    .mgm a,
+    .mgm a:active {
+        transition: all .2s ease;
+        color: #fff;
+    }
+</style>
+<div class="mgm" style="display: none;">
+    <div class="txt" style="color:#eeeeee;">Investor from <b></b> just earned <a href="javascript:void(0);" onclick="javascript:void(0);"></a></div>
+</div>
+</div>
+<script type="text/javascript">
+    // var listCountries = ['United Kingdom', 'India', 'Philippine', 'USA', 'Germany', 'France', 'Italy', 'South Korea', 'Australia', 'Norway', 'Canada', 'Argentina', 'Saudi Arabia', 'Mexico', 'Spain', 'Austria', 'Venezuela', 'South Africa', 'Sweden', 'Botswana', 'South Korea', 'China', 'Italy', 'Germany', 'United Kingdom', 'Bahrain', 'Greece', 'Cuba', 'Bulgaria', 'Portugal', 'Austria', 'Cyprus', 'Panama', 'Asia', 'Norway', 'Netherlands', 'Switzerland', 'Belgium', 'Israel', 'Cyprus','Spain','Norway'];
+    // var listPlans = ['$2000','$1000','$50000','$15000','$10000','$100000','$20000','$1500','$30000','$40000','$1200','$60000','$3400', '$7000', '$25000', '$25000', '$35000', '$5000','$7000','$45000','$3500','$5000','$12500','$9500'];
+    // interval = Math.floor(Math.random() * (40000 - 8000 + 1) + 8000);
+    // var run = setInterval(request, interval);
+
+    // function request() {
+    //     clearInterval(run);
+    //     interval = Math.floor(Math.random() * (40000 - 8000 + 1) + 8000);
+    //     var country = listCountries[Math.floor(Math.random() * listCountries.length)];
+    //     var plan = listPlans[Math.floor(Math.random() * listPlans.length)];
+    //     var msg = 'Someone from <b>' + country + '</b> just earned  <a href="javascript:void(0);" onclick="javascript:void(0);">' + plan + ' .</a>';
+    //     $(".mgm .txt").html(msg);
+    //     $(".mgm").stop(true).fadeIn(300);
+    //     window.setTimeout(function() {
+    //         $(".mgm").stop(true).fadeOut(300);
+    //     }, 6000);
+    //     run = setInterval(request, interval);
+    // }
+</script>
+<!-- Wrapper Ends -->
+
+<script src="assets/js/styleswitcher.js"></script>
+<script type="text/javascript">
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'en'
+        }, 'google_translate_element');
+    }
+</script>
+
+<script type="text/javascript" src="../translate.google.com/translate_a/elementa0d8.js?cb=googleTranslateElementInit"></script>
+
+</body>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '77527bcf7808b15a219eb1e231d86ef839a7bc62';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+
+
+
+<!-- Mirrored from priortradeoption.org/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 06 Jun 2023 06:52:54 GMT -->
+
+</html>
---
+++ layout/header.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from priortradeoption.org/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 06 Jun 2023 06:52:24 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="TGsNneKG8nuKuIV1Pwjtj4Wn0hGYV3WJt89rYhdM">
+
+  <meta charset="utf-8" />
+  <title>FXTM CAPITAL GROWTH</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+  <!-- Favicon -->
+  <link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+  <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+  <!-- <meta name="description" content="FXTM CAPITAL GROWTH is a trusted paying binary and Cryptocurrency trading company. Earn high returns from our proven trading strategies.">
+  <meta name="title" content="FXTM CAPITAL GROWTH">
+  <meta name="keywords" content="FXTM CAPITAL GROWTH, FXTM CAPITAL GROWTH, priortradeoption.org , binary trading, Broker, forex trading, Investment, forex investment, Trusted investment platforms, Egypt investment platforms,  US investment platforms, UK investment platforms, Paying websites, investment websites, trusted investment platform.">
+  <meta name="robots" content="index"> -->
+
+  <!-- Open Graph / Facebook -->
+  <!-- <meta property="og:type" content="website">
+  <meta property="og:url" content="https://priortradeoption.org ">
+  <meta property="og:title" content="FXTM CAPITAL GROWTH">
+  <meta property="og:description" content="FXTM CAPITAL GROWTH is a trusted paying binary and Cryptocurrency trading company. Earn high returns from our proven trading strategies.">
+  <meta property="og:image" content="assets/images/pp.jpg"> -->
+
+  <!-- Twitter -->
+  <!-- <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://priortradeoption.org ">
+  <meta property="twitter:title" content="FXTM CAPITAL GROWTH">
+  <meta property="twitter:description" content="FXTM CAPITAL GROWTH is a trusted paying binary and cryptocurrency trading company. Earn high returns from our proven trading strategies.">
+  <meta property="twitter:image" content="assets/images/pp.jpg"> -->
+
+  <link rel="manifest" href="manifest.json">
+
+  <!-- <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="application-name" content="FXTM CAPITAL GROWTH">
+  <meta name="apple-mobile-web-app-title" content="FXTM CAPITAL GROWTH">
+  <meta name="theme-color" content="#e8b90e">
+  <meta name="msapplication-navbutton-color" content="#e8b90e">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta name="apple-mobile-web-app-capable" content="yes"> -->
+  <link href="assets/images/appicons/ios/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+  <link href="assets/images/appicons/ios/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+  <link href="assets/images/appicons/ios/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+  <link href="assets/images/appicons/ios/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+  <link href="assets/images/appicons/ios/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+  <link href="assets/images/appicons/ios/iphonexsmax_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+  <link href="assets/images/appicons/ios/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+  <link href="assets/images/appicons/ios/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+  <link href="assets/images/appicons/ios/ipadpro3_splash.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+  <link href="assets/images/appicons/ios/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+
+
+  <!-- Template CSS Files -->
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/magnific-popup.css">
+  <link rel="stylesheet" href="assets/css/owl.carousel.css">
+  <link rel="stylesheet" href="assets/css/owl.theme.default.css">
+  <link rel="stylesheet" href="assets/css/select2.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/skins/blue.css">
+
+  <!-- Live Style Switcher - demo only -->
+  <!-- <link rel="alternate stylesheet" type="text/css" title="orange" href="/assets/css/skins/orange.css" disabled="" />
+    <link rel="alternate stylesheet" type="text/css" title="green" href="/assets/css/skins/green.css" disabled="" />
+    <link rel="alternate stylesheet" type="text/css" title="blue" href="/assets/css/skins/blue.css" disabled="" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/styleswitcher.css" /> -->
+
+  <!-- Template JS Files -->
+  <script src="assets/js/modernizr.js"></script>
+
+
+</head>
+
+<!--Start of Tawk.to Script-->
+<!--End of Tawk.to Script-->
+
+<body data-mobile-header-scheme="#e8b90e">
+  <!-- SVG Preloader Starts -->
+  <!-- <div id="preloader">
+        <div><img src="/assets/images/logo.png" style="" alt="Loading..."></div>
+    </div> -->
+
+
+
+  <div class="wrapper">
+    <!-- Header Starts -->
+    <header class="header">
+      <div class="col-md-12" style="padding: 0 !important;">
+        <!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container mt-on-mobile">
+          <div class="tradingview-widget-container__widget"></div>
+          <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+            {
+              "symbols": [{
+                  "proName": "FOREXCOM:SPXUSD",
+                  "title": "S&P 500"
+                },
+                {
+                  "proName": "FOREXCOM:NSXUSD",
+                  "title": "US 100"
+                },
+                {
+                  "proName": "FX_IDC:EURUSD",
+                  "title": "EUR/USD"
+                },
+                {
+                  "proName": "BITSTAMP:BTCUSD",
+                  "title": "Bitcoin"
+                },
+                {
+                  "proName": "BITSTAMP:ETHUSD",
+                  "title": "Ethereum"
+                },
+                {
+                  "description": "Tesla",
+                  "proName": "NASDAQ:TSLA"
+                },
+                {
+                  "description": "Meta",
+                  "proName": "NASDAQ:FB"
+                },
+                {
+                  "description": "Crude Oil",
+                  "proName": "NYMEX:CL1!"
+                }
+              ],
+              "showSymbolLogo": true,
+              "colorTheme": "dark",
+              "isTransparent": false,
+              "displayMode": "adaptive",
+              "locale": "en"
+            }
+          </script>
+
+        </div>
+
+
+        <div class="tradingview-widget-container hide-on-mobile">
+          <div class="tradingview-widget-container__widget"></div>
+          <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
+            {
+              "symbols": [{
+                  "proName": "FOREXCOM:SPXUSD",
+                  "title": "S&P 500"
+                },
+                {
+                  "proName": "FOREXCOM:NSXUSD",
+                  "title": "US 100"
+                },
+                {
+                  "proName": "FX_IDC:EURUSD",
+                  "title": "EUR/USD"
+                },
+                {
+                  "proName": "BITSTAMP:BTCUSD",
+                  "title": "Bitcoin"
+                },
+                {
+                  "proName": "BITSTAMP:ETHUSD",
+                  "title": "Ethereum"
+                },
+                {
+                  "description": "",
+                  "proName": "OANDA:XAUUSD"
+                },
+                {
+                  "description": "",
+                  "proName": "TVC:DXY"
+                }
+              ],
+              "colorTheme": "dark",
+              "isTransparent": false,
+              "showSymbolLogo": true,
+              "locale": "en"
+            }
+          </script>
+        </div>
+
+      </div>
+      <style>
+        .tradingview-widget-container {
+          overflow: hidden;
+        }
+
+        .tradingview-widget-container>iframe>body>.tv-embed-widget-wrapper__body>.js-copyright-label {
+          display: none !important;
+        }
+      </style>
+      <div class="container">
+
+        <div class="row">
+          <!-- Logo Starts -->
+          <div class="main-logo col-xs-12 col-md-3 col-md-2 col-lg-2 hidden-xs">
+            <!-- <a href="index.html">
+                        <img id="logo" class="img-responsive" src="assets/images/logo.png" alt="logo" height="100%" width="100%">
+                    </a> -->
+          </div>
+          <!-- Logo Ends -->
+          <!-- Statistics Starts -->
+          <div class="col-md-7 col-lg-7">
+
+            <ul class="unstyled bitcoin-stats text-center">
+              <li>
+                <h6>10,426 USD</h6><span>Last trade price</span>
+              </li>
+              <li>
+                <h6>6,205,780 USD</h6><span>24 hour volume</span>
+              </li>
+              <li>
+                <h6>24K+</h6><span style="color:green;">active users</span>
+              </li>
+              <li>
+                <div class="btcwdgt-price" data-bw-theme="light" data-bw-cur="usd"></div>
+                <span>Live Bitcoin price</span>
+              </li>
+            </ul>
+          </div>
+
+          <!-- Statistics Ends -->
+          <!-- User Sign In/Sign Up Starts -->
+          <div class="col-md-3 col-lg-3">
+            <ul class="unstyled user">
+              <li class="sign-in"><a href="https://dashboard.fxtm-capitalgrowth.com/login" class="btn btn-primary">sign in</a></li>
+              <li class="sign-up"><a href="https://dashboard.fxtm-capitalgrowth.com/register" class="btn btn-primary">register</a></li>
+            </ul>
+          </div>
+          <!-- User Sign In/Sign Up Ends -->
+        </div>
+      </div>
+      <!-- Navigation Menu Starts -->
+      <nav class="site-navigation navigation" id="site-navigation">
+        <div class="container">
+          <div class="site-nav-inner">
+            <!-- Logo For ONLY Mobile display Starts -->
+            <a class="logo-mobile" href="index.html">
+              <img id="logo-mobile" class="img-responsive" src="assets/images/logo.png" alt="FXTM CAPITAL GROWTH">
+            </a>
+            <!-- Logo For ONLY Mobile display Ends -->
+            <!-- Toggle Icon for Mobile Starts -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <!-- Toggle Icon for Mobile Ends -->
+            <div class="collapse navbar-collapse navbar-responsive-collapse">
+              <!-- Main Menu Starts -->
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <!-- <li><a href="packages.php">Packages</a></li> -->
+                <li><a href="contact.php">Contact</a></li>
+                <li>
+                  <ul class="unstyled user hidden visible-xs">
+                    <li class="sign-in"><a href="login.html" class="btn btn-primary">sign in</a></li>
+                    <li class="sign-up"><a href="register.html" class="btn btn-primary">register</a></li>
+                  </ul>
+                </li>
+                <!-- Cart Icon Starts -->
+
+                <!-- Cart Icon Starts -->
+                <!-- Search Icon Starts -->
+                <!-- Search Icon Ends -->
+              </ul>
+              <!-- Main Menu Ends -->
+            </div>
+          </div>
+        </div>
+        <!-- Search Input Starts -->
+        <div class="site-search">
+          <div class="container">
+            <input type="text" placeholder="type your keyword and hit enter ...">
+            <span class="close">×</span>
+          </div>
+        </div>
+        <!-- Search Input Ends -->
+      </nav>
+      <!-- Navigation Menu Ends -->
+    </header>
